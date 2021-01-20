35ec5c4b821986b02bf86fdd6dda9b6e774ad995
juliantjg <juliantj88@gmail.com>
2021-01-20 18:14:07 +1100

    Fixed profile

--- cinemaProject/resources/views/profiles/index.blade.php
+++ cinemaProject/resources/views/profiles/index.blade.php
@@ -19,19 +19,16 @@
             </div>
         </div>
         <div class="col-4 pt-4">
-            <div class="row">
-                <h1 style="color:white;"> {{ $user->name }} </h1> 
+            <div class="row d-flex">
+                <h1 style="color:white;"> <b>{{ $user->name }} </b></h1>   
+                <div class="pt-2 pl-4">
+                    <h3 style="color: white;">< {{ ucfirst($user->role) }} ></h3>
+                </div>
             </div>
             <div class="row pt-4">
-                <h4 style="color:white;"> {{ ucfirst($user->role) }} </h2>
-            </div>
-            <div class="row">
                 <h4 style="color:white;"> {{ $user->profile->description ?? '' }} </h2>
             </div>
             
-        </div>
-        <div class="col-4">
-            <h1 style="color: white;">{{ $user->profile->user_id ?? '' }}</h1>
         </div>
     </div>
 </div>
