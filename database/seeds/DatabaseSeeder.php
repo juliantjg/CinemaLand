d2e2207484227dd80eb03a8225284f050abe9eda
juliantjg <juliantj88@gmail.com>
2021-02-04 15:54:55 +1100

    Seeder for menu

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('menus')->insert([
+            'id' => '1',
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
