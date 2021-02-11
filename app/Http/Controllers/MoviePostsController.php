4dc1ab26ee4900c23d123d3980d3233912ea4336
juliantjg <juliantj88@gmail.com>
2021-02-11 14:44:47 +1100

    Search available for non user

--- app/Http/Controllers/MoviePostsController.php
+++ app/Http/Controllers/MoviePostsController.php
@@ -11,7 +11,7 @@ class MoviePostsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('search');
         $this->middleware('admin')->except('show', 'search');
     }
 
